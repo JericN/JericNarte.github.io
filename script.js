cc19a73e5bc093ce4dc0f6099a564adc9b4fa50e
Jeric <62868605+JericNarte@users.noreply.github.com>
2022-10-01 13:47:32 +0800

    axios

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ var data1 = document.querySelector('#data1')
 var data2 = document.querySelector('#data2')
 var data3 = document.querySelector('#data3')
 
+var user_name
+
 data1.innerHTML = '3'
 btn.addEventListener('click', () => {
 	userAction()
@@ -11,7 +13,14 @@ btn.addEventListener('click', () => {
 
 const userAction = async () => {
 	console.log('here')
-	const response = await fetch('https://api.henrikdev.xyz/valorant/v1/account/soda/XVII')
-	const myJson = await response.json()
-	console.log(myJson)
+	// const response = await fetch('https://api.henrikdev.xyz/valorant/v3/matches/ap/soda/XVII?filter=competitive')
+	// const profile_data = await response.json()
+	// console.log(profile_data)
 }
+const axios = require('axios')
+
+;(async () => {
+	const { data } = await axios.get('https://async.scraperapi.com/jobs/0962a8e0-5f1a-4e14-bf8c-5efcc18f0953')
+
+	console.log(data)
+})()
